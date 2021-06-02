ab2f0bc13d0a519c79467d5093d078f1f2363d85
msl8r <72858053+msl8r@users.noreply.github.com>
2021-06-02 12:01:11 +0100

    Merge pull request #1 from hmcts/DTSPO-379_policy_manager
    DTSPO-379 - add policy manager and initial policy and assignment

---
+++ pipeline-scripts/sandbox-override.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export ASSIGNMENTS=$(find ./assignments -type f -name 'assign.*.json')
+export POLICIES=$(find ./policies -name policy.json -type f )
+export ENVIRONMENT=${ENVIRONMENT:-Sandbox}
+export ASSIGNMENTS_DIR="./${ENVIRONMENT:-Sandbox}/assignments"
+export POLICIES_DIR="./${ENVIRONMENT:-Sandbox}/policies"
+
+mkdir -p ${ASSIGNMENTS_DIR} ${POLICIES_DIR}
+
+
+echo "Creating Sandbox Policies"
+for policy in ${POLICIES}; do 
+    FILE=$(basename ${policy})
+    DIR=$(basename "$(dirname ${policy})" )
+    mkdir -p ${POLICIES_DIR}/${DIR}
+
+    echo "Creating file: ${POLICIES_DIR}/${DIR}/${FILE}"    
+    npx json -f ${policy} \
+    -e 'this.name=this.name + process.env.ENVIRONMENT' \
+    -e 'this.properties.displayName=this.properties.displayName + " - " + process.env.ENVIRONMENT ' \
+    -e 'this.id=process.env.SUB + "/providers/Microsoft.Authorization/policyDefinitions/" + this.name' > ${POLICIES_DIR}/${DIR}/${FILE}
+
+done
+
+echo "Creating Sandbox Assignments"
+for assignment in ${ASSIGNMENTS}; do 
+    FILE=$(basename ${assignment})
+    DIR=$(basename "$(dirname ${assignment})" )
+    mkdir -p ${ASSIGNMENTS_DIR}/${DIR}
+
+    echo "Creating file: ${ASSIGNMENTS_DIR}/${DIR}/${FILE}"
+    npx json -f ${assignment} \
+    -e 'this.properties.scope=process.env.SUB' \
+    -e 'this.name=this.name + process.env.ENVIRONMENT' \
+    -e 'this.properties.displayName=this.properties.displayName + " - " + process.env.ENVIRONMENT ' \
+    -e 'this.properties.policyDefinitionId=process.env.SUB + "/providers/Microsoft.Authorization/policyDefinitions/" + this.properties.policyDefinitionId.split("/").pop() + process.env.ENVIRONMENT' \
+    -e 'this.id=process.env.SUB + "/providers/Microsoft.Authorization/policyAssignments/" + this.name' > ${ASSIGNMENTS_DIR}/${DIR}/${FILE}
+
+done
---
+++ policies/tagging/README.md
@@ -0,0 +1,33 @@
+# HMCTS Tagging Policy 
+
+## Required tags
+Required tags, common and specific to subscription of resource.
+
+All environments and resources: 
+ - "environment"
+ - "application"
+ - "businessArea"
+ - "builtFrom"
+     
+## Allowed values
+Tags and their allowed values.
+
+application:
+ - "core"
+
+environment:
+ - "sandbox"
+ - "development"
+ - "testing"
+ - "demo"
+ - "ITHC"
+ - "staging"
+ - "production"
+ 
+businessArea:
+ - "CFT"
+ - "crime"
+ - "cross-cutting"
+ 
+ builtFrom: 
+ - Name or URL of repository
