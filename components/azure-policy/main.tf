c0e17006081f8c94a5b51a26cb7a59676c1ac018
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-21 11:50:02 +0100

    Initial terraform

---
+++ components/azure-policy/main.tf
@@ -0,0 +1,6 @@
+# Collect all .json.tpl files under the policies directories
+# Render all templates according to current environment
+# Create policy definition for current env
+
+# Collect all .json.tpl files under assignments/subscriptions/* 
+# Create a policy assignment for each file
