9f7ce51c47a6030d626121864db72fa9f19d3d22
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-22 11:47:51 +0100

    Run linter

--- components/azure-policy/locals.tf
+++ components/azure-policy/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  policy_dir = "../../policies/"
+  policy_dir       = "../../policies/"
   subscription_dir = "../../assignments/${var.tenant}/subscriptions/"
-  mgmt_group_dir = "../../assignments/${var.tenant}/mgmt-groups/"
+  mgmt_group_dir   = "../../assignments/${var.tenant}/mgmt-groups/"
 
   policy_files = [
     for policy in fileset(local.policy_dir, "**/*.json") :
