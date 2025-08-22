1ced0ca7c48ff646a4c942928035d12c8ebe858f
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-22 11:34:06 +0100

    Add variables file

---
+++ components/variables.tf
@@ -0,0 +1,29 @@
+variable "env" {
+  default = "sbox"
+}
+
+variable "product" {
+  default = "azure-policy"
+}
+
+variable "location" {
+  default = "uksouth"
+}
+
+variable "builtFrom" {
+  default = "hmcts/azure-policy"
+}
+
+variable "tenant" {
+  default = "CNP_SBOX"
+}
+
+variable "management_group" {
+  type    = optional(string)
+  default = "/providers/Microsoft.Management/managementGroups/HMCTS"
+}
+
+variable "name_suffix" {
+  type    = optional(string)
+  default = ""
+}
