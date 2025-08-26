026271bea87847058c8c7d24f0c529cd5a312a37
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-26 09:28:00 +0100

    Scuff it

--- components/azure-policy/import.tf
+++ components/azure-policy/import.tf
@@ -1,5 +1,5 @@
 import {
-  for_each = var.env == "sbox" ? local.policies : {}
+  for_each = [local.policies, {}][var.env == "sbox" ? 0 : 1]
   to       = azurerm_policy_definition.policies["each.key"]
   id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/${each.key}"
 }
