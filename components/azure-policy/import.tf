0eaa911f6fd7531b348e5022ad0742ab94386d7d
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-26 09:15:39 +0100

    Update import

--- components/azure-policy/import.tf
+++ components/azure-policy/import.tf
@@ -1,23 +1,5 @@
 import {
-  for_each = var.env == "sbox" ? 1 : 0
-  to       = azurerm_policy_definition.policies["ExpiresAfterTagging"]
-  id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/ExpiresAfterTagging"
-}
-
-import {
-  for_each = var.env == "sbox" ? 1 : 0
-  to       = azurerm_policy_definition.policies["HMCTSAGWDiagnostic"]
-  id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/HMCTSAGWDiagnostic"
-}
-
-import {
-  for_each = var.env == "sbox" ? 1 : 0
-  to       = azurerm_policy_definition.policies["HMCTSAutoTagging"]
-  id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/HMCTSAutoTagging"
-}
-
-import {
-  for_each = var.env == "sbox" ? 1 : 0
-  to       = azurerm_policy_definition.policies["HMCTSAzureSQLAudit"]
-  id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/HMCTSAzureSQLAudit"
+  for_each = var.env == "sbox" ? local.policies : []
+  to       = azurerm_policy_definition.policies["each.key"]
+  id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/${each.key}"
 }
