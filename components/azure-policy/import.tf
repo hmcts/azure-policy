ce3cd38bd776d9cefe552c194c964fd544c3ea23
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-26 09:31:27 +0100

    Fix import key

--- components/azure-policy/import.tf
+++ components/azure-policy/import.tf
@@ -1,5 +1,5 @@
 import {
   for_each = [local.policies, {}][var.env == "sbox" ? 0 : 1]
-  to       = azurerm_policy_definition.policies["each.key"]
+  to       = azurerm_policy_definition.policies[each.key]
   id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/${each.key}"
 }
