80165de0304095f5aa4526af49df4846ceeafef6
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-26 09:50:50 +0100

    Update import id

--- components/azure-policy/import.tf
+++ components/azure-policy/import.tf
@@ -1,5 +1,5 @@
 import {
   for_each = [local.policies, {}][var.env == "sbox" ? 0 : 1]
   to       = azurerm_policy_definition.policies[each.key]
-  id       = "/providers/Microsoft.Management/managementGroups/HMCTS/providers/Microsoft.Authorization/policyDefinitions/${each.key}"
+  id       = "/subscriptions/b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb/providers/Microsoft.Authorization/policyDefinitions/${each.key}"
 }
