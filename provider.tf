e2bc4035a2cc9fb363daa498c0050cf618d77b7f
Simran Sian <simran.sian1@justice.gov.uk>
2025-08-29 09:57:47 +0100

    Update provider

--- provider.tf
+++ provider.tf
@@ -2,7 +2,10 @@ terraform {
   required_version = ">= 1.12.2"
 
   backend "azurerm" {
+    container_name = "cpp-azure-policy"
+    key            = "terraform.tfstate"
   }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
