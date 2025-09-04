44a351829a7d66bcf4ee9f0780dfb900d1c5884b
Dawid Strozak <dawid.strozak@hmcts.net>
2025-09-04 16:40:09 +0100

    Fix new policies by removing the non-compliance messages as they are not supported for these policies Change load balancers policy to DENY as we are already compliant with this after checking sandbox cluster Change no default namsepaces policy to Deny as it does not have "DeployIfNotExists" effect Add userAssigned identity config to cluster command invoke policy as this is required for remediation when using "deployIfNotExists" effect Modify sandbox-override.sh script so that it supports testing from a PR of assignments for both newly added custom policies as well as built-in azure policies

--- pipeline-scripts/sandbox-override.sh
+++ pipeline-scripts/sandbox-override.sh
@@ -2,6 +2,9 @@
 set -ex
 
 export SUB_ASSIGNMENTS=$(find ./assignments/$SUB  -type f -name 'assign.*.json')
+# Where we want to test assignment of policies which are already built into Azure process those separately
+# Without modifying the policyDefinitionId to point to a custom created policy definition
+export BUILTIN_SUB_ASSIGNMENTS=$(find ./assignments/$SUB -type f -name 'builtin.assign.*.json')
 export MGMT_ASSIGNMENTS=$(find ./assignments/mgmt-groups/mg-cft-sandbox -type f -name 'assign.*.json')
 export POLICIES=$(find ./policies -name policy.json -type f )
 export ENVIRONMENT=${ENVIRONMENT:-Sandbox}
@@ -25,21 +28,42 @@ for policy in ${POLICIES}; do
 
 done
 
-echo "Creating Sandbox Subscription Assignments"
-for assignment in ${SUB_ASSIGNMENTS}; do
+process_subscription_assignment() {
+    assignment=$1
+    built_in_assignment=$2
+
     FILE=$(basename ${assignment})
     DIR=$(basename "$(dirname ${assignment})" )
     mkdir -p ${ASSIGNMENTS_DIR}/${DIR}
+    if [ "$built_in_assignment" = true ]; then
+        echo "Creating file for built-in policy: ${ASSIGNMENTS_DIR}/${DIR}/${FILE}"
+        CUSTOM_POLICY_DEFINITION_ID=""
+        npx json -f ${assignment} \
+        -e 'this.properties.scope=process.env.SUB' \
+        -e 'this.name=this.name + "_" + process.env.ENVIRONMENT' \
+        -e 'this.properties.displayName=this.properties.displayName + " - " + process.env.ENVIRONMENT ' \
+        -e 'this.properties.policyDefinitionId=this.properties.policyDefinitionId' \
+        -e 'this.properties.notScopes=[]' \
+        -e 'this.id=process.env.SUB + "/providers/Microsoft.Authorization/policyAssignments/" + this.name' > ${ASSIGNMENTS_DIR}/${DIR}/${FILE}
+    else
+        echo "Creating file for custom policy: ${ASSIGNMENTS_DIR}/${DIR}/${FILE}"
+        npx json -f ${assignment} \
+        -e 'this.properties.scope=process.env.SUB' \
+        -e 'this.name=this.name + "_" + process.env.ENVIRONMENT' \
+        -e 'this.properties.displayName=this.properties.displayName + " - " + process.env.ENVIRONMENT ' \
+        -e 'this.properties.policyDefinitionId=process.env.SUB + "/providers/Microsoft.Authorization/policyDefinitions/" + this.properties.policyDefinitionId.split("/").pop() + process.env.ENVIRONMENT' \
+        -e 'this.properties.notScopes=[]' \
+        -e 'this.id=process.env.SUB + "/providers/Microsoft.Authorization/policyAssignments/" + this.name' > ${ASSIGNMENTS_DIR}/${DIR}/${FILE}
+    fi
+}
 
-    echo "Creating file: ${ASSIGNMENTS_DIR}/${DIR}/${FILE}"
-    npx json -f ${assignment} \
-    -e 'this.properties.scope=process.env.SUB' \
-    -e 'this.name=this.name + "_" + process.env.ENVIRONMENT' \
-    -e 'this.properties.displayName=this.properties.displayName + " - " + process.env.ENVIRONMENT ' \
-    -e 'this.properties.policyDefinitionId=process.env.SUB + "/providers/Microsoft.Authorization/policyDefinitions/" + this.properties.policyDefinitionId.split("/").pop() + process.env.ENVIRONMENT' \
-    -e 'this.properties.notScopes=[]' \
-    -e 'this.id=process.env.SUB + "/providers/Microsoft.Authorization/policyAssignments/" + this.name' > ${ASSIGNMENTS_DIR}/${DIR}/${FILE}
+echo "Creating Sandbox Subscription Assignments"
+for assignment in ${SUB_ASSIGNMENTS}; do
+    process_subscription_assignment "$assignment" false
+done
 
+for assignment in ${BUILTIN_SUB_ASSIGNMENTS}; do
+    process_subscription_assignment "$assignment" true
 done
 
 ## Commented out for now, this will not work with policies created at subscription level as we do above (line 15).
